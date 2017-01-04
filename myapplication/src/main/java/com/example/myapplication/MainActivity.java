616e3d93e393bd667689f280bd33112af33ee15c
gejun <917609824@qq.com>
2017-01-04 11:59:51 +0800

    my contentprovider

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Cursor cursor = getContentResolver().query(Uri.parse("content://com.example.dbdemo.provider/book_table/7"), null, null, null, null);
+        if(cursor != null){
+            while(cursor.moveToNext()){
+                Log.i("gejun","author = " + cursor.getString(cursor.getColumnIndex("author"))
+                +", name = " + cursor.getString(cursor.getColumnIndex("name"))
+                +", price = " + cursor.getString(cursor.getColumnIndex("price")));
+            }
+        }
     }
 }
