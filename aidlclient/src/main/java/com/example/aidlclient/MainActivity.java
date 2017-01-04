0d6e6d182672cd8f9d12c1a108ecfbe0fab4809d
gejun <917609824@qq.com>
2017-01-04 12:26:34 +0800

    aidl client

---
+++ aidlclient/src/main/java/com/example/aidlclient/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.aidlclient;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.aidl.IMyAidlInterface;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent it = new Intent();
+        it.setAction("com.aidl.server");
+        it.setPackage("com.example.aidlserver");
+        bindService(it, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unbindService(mConnection);
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            IMyAidlInterface aidl = IMyAidlInterface.Stub.asInterface(service);
+            try {
+                Log.i("gejun","a + b = " + aidl.add(3, 4));
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    private Handler mHandler = new Handler(){
+
+    };
+}
